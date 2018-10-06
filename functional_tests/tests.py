6622e7b1ca8c7e07d21215747b904219a4f87437
paulmattei <paulmattei@protonmail.com>
2018-10-06 21:55:41 +0100

    removed unnecessary sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -61,7 +61,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and nw shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
